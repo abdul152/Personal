9c1bc8678f6260ad8739b3092215c2c10cf14dd6
Abdul-Samad <Abdul-Samad@ABDUL-SAMAD>
2019-11-02 19:26:32 +0100

    test

---
+++ Personal-Project/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Personal_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
