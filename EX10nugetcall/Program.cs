cf8fe0343c6a7d88ac9209c990a7366345a1bcdb
Abdul-Samad <Abdul-Samad@ABDUL-SAMAD>
2019-12-01 15:00:23 +0100

    commit

---
+++ EX10nugetcall/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EX10nugetcall
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ EX11/Calculator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace EX11
+{
+   public class Calculator
+    {
+
+        public static int Add(int x, int y)
+        {
+            int z = x + y;
+
+
+            return (z);
+        }
+
+
+        public static int Divide(int a, int b)
+        {
+            int P = a / b;
+
+            return (P);
+        }
+
+        static void Main(string[] args)
+        {
+            
+        }
+
+        
+    }
+}
